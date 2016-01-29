17daa4f46d068e5f83dc14206521b9181457a5ea
next_sentence <petru_binzari@yahoo.com>
2016-01-29 18:38:12 +0200

    add new field to product entity

---
+++ src/AppBundle/Form/Type/ProductType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AppBundle\Form\Type;
+
+use AppBundle\Entity\Product;
+use Sylius\Bundle\CoreBundle\Form\Type\ProductType as BaseProductType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class ProductType extends BaseProductType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+
+        $builder
+            ->add('type', 'choice', array(
+                'label'   => 'Type',
+                'choices' => Product::getTypeLabels(),
+                'expanded' => true,
+            ))
+        ;
+    }
+}
